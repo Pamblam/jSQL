b8cb624e543092a13bc58e93ab03b86c1c27f1a8
Robert Parham <adelphia@protonmail.ch>
2017-10-13 17:17:37 -0400

    working on JMA

--- plugins/jSQLMyAdmin/userscript.jSQLMyAdmin.js
+++ plugins/jSQLMyAdmin/userscript.jSQLMyAdmin.js
@@ -6,9 +6,9 @@
 // @version     1.6
 // @grant       none
 // ==/UserScript==
-void(function () {
-	var jSQLMyAdminVersion = 1.6;
-	
+void(function(){
+	const jSQLMyAdminVersion = 1.6;
+
 	/**
 	 * Turn this userscript into a browser button..
 	 *  - Change the isBtn variable below to true
@@ -18,17 +18,16 @@ void(function () {
 	 *		<a href="javascript:(minified, urlencoded userscript here)">jSQLMyAdmin</a>
 	 *  - Open HTML page in browser, drag link to bookmarks toolbar
 	 */
-	var isBtn = false;
-	
-	var cm;
-	var drawnTables = [];
-	var resources = [];
-	var minJSQLVersion = 2.7;
-	
+	let isBtn = false;
+
+	let cm;
+	let drawnTables = [];
+	let resources = [];
+	const minJSQLVersion = 2.7;
+
 	if (typeof window.jSQL === 'undefined'){
 		resources.push({
-			// jSQL edge
-			js: ['https://gitcdn.xyz/repo/Pamblam/jSQL/master/jSQL.min.js'],
+			js: ['https://pamblam.github.io/jSQL/scripts/jSQL.min.js'],
 			css: []
 		});
 	}
@@ -53,24 +52,24 @@ void(function () {
 		css: ['https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.22.0/codemirror.css',
 			'https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.22.0/addon/hint/show-hint.css'],
 	});
-	
+
 	showjSQLLoader();
-	loadresources(resources).then(function(){
+	loadresources(resources).then(()=>{
 		if("undefined" === typeof jSQL.version || jSQL.version < minJSQLVersion){
-			var current_version = "undefined" === typeof jSQL.version ? "<1" : jSQL.version;
+			let current_version = "undefined" === typeof jSQL.version ? "<1" : jSQL.version;
 			if(!confirm("jSQLMyAdmin requires jSQL version >= "+minJSQLVersion+". This page includes jSQL version "+current_version+". Some features will not work. Do you want to open jSQLMyAdmin anyway?"))
 				return;
 		}
 		closejSQL();
 		if(isBtn) openjSQL();
 	});
-	
+
 	function showjSQLLoader(){
 		document.getElementsByTagName('body')[0]
 			.insertAdjacentHTML('beforeend','<span id=\'jSQLLoadingFlag\' style=\'position:fixed; top:5px; right:5px; z-index:2147483647; opacity:0.7;\'>Loading jSQLMyAdmin...</span>');
 	}
 	function drawjSQLMyAdmin() {
-		jSQL.load(function () {
+		jSQL.load(()=>{
 			// does the saved queries table exist?
 			if(undefined === jSQL.tables['Saved Queries']){
 				jSQL.query("CREATE TABLE `Saved Queries` (`Name` VARCHAR(20), `Query` LONGTEXT)").execute();
@@ -80,36 +79,90 @@ void(function () {
 				jSQL.insertInto("Saved Queries").values({Name:"Insert", Query: "-- An Insert Query Template\nINSERT INTO `myTable`\n\t(`String`, `Number`)\nVALUES\n\t('Some Number', 7)"}).execute();
 				jSQL.insertInto("Saved Queries").values({Name:"Drop", Query: "-- A Drop Query Template\nDROP TABLE `myTable`"}).execute();
 			}
-			var queries = jSQL.query("SELECT * FROM `Saved Queries`").execute().fetchAll("ASSOC");
-			var selectMenu = "<select id='queryTypeSelect'><option>Choose...</option>";
-			for(var i=queries.length; i--;) selectMenu += "<option value='"+queries[i].Name+"'>"+queries[i].Name+"</option>";
+			let queries = jSQL.query("SELECT * FROM `Saved Queries`").execute().fetchAll("ASSOC");
+			let selectMenu = "<select id='queryTypeSelect'><option>Choose...</option>";
+			for(let i=queries.length; i--;) selectMenu += "<option value='"+queries[i].Name+"'>"+queries[i].Name+"</option>";
 			selectMenu += "</select>";
-			var $overlay = $('#jsOverlayContainer').empty();
+			let $overlay = $('#jsOverlayContainer').empty();
 			$overlay.html('<h5>jSQL Version: ' + jSQL.version + ' | jSQLMyAdmin Version: ' + jSQLMyAdminVersion + '</h5>');
-			$overlay.append('<div id="jSQLTableTabs"><ul><li><a href="#jSQLResultsTab">Query</a></li></ul><div id="jSQLResultsTab"><div><div style="float:right;"><small><b>autocomplete</b>: <i>ctrl+space</i></small></div><div style="float:left"><b>Template: </b>'+selectMenu+'</div><div style="clear:both;"></div></div><textarea id="jSQLMyAdminQueryBox"></textarea><div style="text-align:right;"><button id="jSQLExecuteQueryButton">Run Query</button><button id="jSQLMinifyQueryButton">Minify</button><button id="jSQLSaveQueryButton">Save</button><button id="jSQLResetButton">Reset</button><button id="jSQLCommitButton">Commit</button></div><div id="jSQLMAQueryResults"><center><b>No results to show</b><br>Enter a query</center></div></div></div>');
+			$overlay.append(`<div id="jSQLTableTabs">
+                                 <ul>
+                                     <li><a href="#jSQLNewTableTab">New Table</a></li>
+                                     <li><a href="#jSQLResultsTab">Query</a></li>
+                                 </ul>
+                                 <div id="jSQLNewTableTab">
+                                     <input type='text' id='tableName' placeholder='Table Name' />
+                                     <table>
+                                         <thead>
+                                             <tr>
+                                                 <th>-Name-</th>
+                                                 <th>-Type-</th>
+                                                 <th>-AI-</th>
+                                                 <th>-PK-</th>
+                                                 <th>-UN-</th>
+                                             </tr>
+                                         </thead>
+                                         <tbody>
+                                             <tr>
+                                                 <td><input type=text class=colname-jma placeholder="Column Name" /></td>
+                                                 <td>
+                                                     <select>
+                                                         <option>NUMERIC</option>
+                                                         <option>TINYINT</option>
+                                                         <option>SMALLINT</option>
+                                                         <option>MEDIUMINT</option>
+                                                         <option>INT</option>
+                                                         <option>BIGINT</option>
+                                                     </select>
+                                                 </td>
+                                                 <td><input type=checkbox class=col-ai-jma /></td>
+                                                 <td><input type=checkbox class=col-pk-jma /></td>
+                                                 <td><input type=checkbox class=col-un-jma /></td>
+                                             </tr>
+                                         </tbody>
+                                     </table>
+                                 </div>
+                                 <div id="jSQLResultsTab">
+                                     <div>
+                                         <div style="float:right;"><small><b>autocomplete</b>: <i>ctrl+space</i></small></div>
+                                         <div style="float:left"><b>Template: </b>${selectMenu}</div>
+                                         <div style="clear:both;"></div>
+                                     </div>
+                                     <textarea id="jSQLMyAdminQueryBox"></textarea>
+                                     <div style="text-align:right;">
+                                         <button id="jSQLExecuteQueryButton">Run Query</button>
+                                         <button id="jSQLMinifyQueryButton">Minify</button>
+                                         <button id="jSQLSaveQueryButton">Save</button>
+                                         <button id="jSQLResetButton">Reset</button>
+                                         <button id="jSQLCommitButton">Commit</button>
+                                     </div>
+                                     <div id="jSQLMAQueryResults"><center><b>No results to show</b><br>Enter a query</center></div>
+                                 </div>
+                             </div>`);
 			$("#queryTypeSelect").change(function(){
-				var Name = $(this).val();
+				let Name = $(this).val();
 				if(undefined === Name || Name === "" || Name === null || Name === false) return;
-				var query = jSQL.query("SELECT `Query` FROM `Saved Queries` WHERE `Name` = ?")
+				let query = jSQL.query("SELECT `Query` FROM `Saved Queries` WHERE `Name` = ?")
 					.execute([Name]).fetch("ASSOC").Query;
 				cm.getDoc().setValue(query);
 				$(this).val($(this).find("option:first").val());
 			});
 			$('#jSQLTableTabs').tabs({
-				beforeActivate: function(event, ui) {
-					var tableName = $(ui.newPanel).data("tn");
+                active: 1,
+				beforeActivate: (event, ui)=>{
+					let tableName = $(ui.newPanel).data("tn");
 					if(tableName === undefined) return;
 					$(ui.newPanel).html("Loading...");
-				}, 
-				activate: function(event, ui){
-					setTimeout(function(){
-						var tableName = $(ui.newPanel).data("tn");
+				},
+				activate: (event, ui)=>{
+					setTimeout(()=>{
+						let tableName = $(ui.newPanel).data("tn");
 						if(tableName === undefined) return;
-						var data = jSQL.select('*').from(tableName).execute().fetchAll('array');
-						var tableHTML = '<div style="overflow-x:auto; width:100%; margin:0; padding:0;"><table></table></div>';
+						let data = jSQL.select('*').from(tableName).execute().fetchAll('array');
+						let tableHTML = '<div style="overflow-x:auto; width:100%; margin:0; padding:0;"><table></table></div>';
 						$(ui.newPanel).html(tableHTML);
-						var cols = [];
-						for(var i=0; i<jSQL.tables[tableName].columns.length; i++) 
+						let cols = [];
+						for(let i=0; i<jSQL.tables[tableName].columns.length; i++)
 							cols.push({title: jSQL.tables[tableName].columns[i]});
 						$(ui.newPanel).find('table').DataTable({
 							data: data,
@@ -127,7 +180,7 @@ void(function () {
 			$("#jSQLMinifyQueryButton").click(minifyjSQLQuery);
 			$("#jSQLResetButton").button({icons: { primary: "ui-icon-refresh"}});
 			$('#jSQLResetButton').css({"line-height": "0.9"});
-			$("#jSQLResetButton").click(function(){
+			$("#jSQLResetButton").click(()=>{
 				if(!confirm("Do you want to delete all tables?")) return;
 				jSQL.reset();
 				jSQL.query("CREATE TABLE `Saved Queries` (`Name` VARCHAR(20), `Query` LONGTEXT)").execute();
@@ -141,27 +194,27 @@ void(function () {
 			});
 			$("#jSQLSaveQueryButton").button({icons: { primary: "ui-icon-star"}});
 			$('#jSQLSaveQueryButton').css({"line-height": "0.9"});
-			$("#jSQLSaveQueryButton").click(function(){
-				var name = prompt("Enter a name for this Template:");
+			$("#jSQLSaveQueryButton").click(()=>{
+				let name = prompt("Enter a name for this Template:");
 				if(!name) return;
-				var query = cm.getValue();
+				let query = cm.getValue();
 				jSQL.query("INSERT INTO `Saved Queries` (`Name`, `Query`) VALUES (?, ?)")
 					.execute([name, query]);
 				$("#queryTypeSelect").append("<option value='"+name+"'>"+name+"</option>");
 			});
 			$("#jSQLCommitButton").button({icons: { primary: "ui-icon-check"}});
 			$('#jSQLCommitButton').css({"line-height": "0.9"});
-			$("#jSQLCommitButton").click(function(){
+			$("#jSQLCommitButton").click(()=>{
 				jSQL.commit();
 				$("#jSQLCommitButton").button("option", "label", "Committed!");
-				setTimeout(function(){
+				setTimeout(()=>{
 					$("#jSQLCommitButton").button("option", "label", "Commit");
 				},2000);
 			});
 			addAllTables();
 			$("head").append("<style>.CodeMirror-hints, .CodeMirror-hint, .CodeMirror-hint-active { z-index:2147483647 !important;}</style>");
-			var tbls = {}; 
-			for (var table in jSQL.tables) tbls[table] = jSQL.tables[table].columns;
+			let tbls = {};
+			for (let table in jSQL.tables) tbls[table] = jSQL.tables[table].columns;
 			cm = CodeMirror.fromTextArea($("#jSQLMyAdminQueryBox")[0], {
 				lineNumbers: true,
 				mode: "text/x-mysql",
@@ -180,14 +233,14 @@ void(function () {
 		});
 	}
 	function minifyjSQLQuery(){
-		var sql = cm.getValue();
+		let sql = cm.getValue();
 		cm.getDoc().setValue(jSQL.minify(sql));
 	}
 	function runjSQLQuery(){
-		var sql = cm.getValue();
+		let sql = cm.getValue();
 		try{
-			var query = jSQL.query(sql).execute();
-			var data = [];
+			let query = jSQL.query(sql).execute();
+			let data = [];
 			if(query!==undefined){
 				data = query.fetchAll("assoc");
 				if(query.type === "DROP"){
@@ -196,20 +249,20 @@ void(function () {
 					cm.getDoc().setValue(sql);
 				}
 			}
-			var tableHTML = data.length ? '<br><div><small><b>Results</b></small></div><div style="overflow-x:auto; width:100%; margin:0; padding:0;">'+makeTableHTML(data)+'</div>' : '<center><b>No results to show</b><br>Enter a query</center>';
+			let tableHTML = data.length ? '<br><div><small><b>Results</b></small></div><div style="overflow-x:auto; width:100%; margin:0; padding:0;">'+makeTableHTML(data)+'</div>' : '<center><b>No results to show</b><br>Enter a query</center>';
 			addAllTables();
 			$("#jSQLMAQueryResults").html(tableHTML);
 			$('#jSQLMAQueryResults').find('table').DataTable({"order": [], "scrollX": true});
 		}catch(e){
-			var msg = e.message ? e.message+"" : e+"";
+			let msg = e.message ? e.message+"" : e+"";
 			alert(msg);
 			throw e;
 		}
 	}
 	function addTableTab(tableName){
 		if(drawnTables.indexOf(tableName)>-1) return;
-		var $ul = $('#jSQLTableTabs').find('ul').eq(0);
-		var id = 'jSQLResultsTab-' + $ul.find('li').length;
+		let $ul = $('#jSQLTableTabs').find('ul').eq(0);
+		let id = 'jSQLResultsTab-' + $ul.find('li').length;
 		$ul.append('<li><a href="#' + id + '">' + tableName + '</a></li>');
 		$ul.after('<div id="' + id + '">Loading...</div>');
 		$("#"+id).data("tn", tableName);
@@ -217,24 +270,24 @@ void(function () {
 		drawnTables.push(tableName);
 	}
 	function addAllTables() {
-		for (var table in jSQL.tables) {
+		for (let table in jSQL.tables) {
 			if("Saved Queries" === table) continue;
 			addTableTab(table);
 		}
 	}
 	function makeTableHTML(data, drawData) {
 		if(undefined === drawData) drawData = true;
-		var html = [];
+		let html = [];
 		html.push('<table><thead><tr>');
-		for (var name in data[0])
+		for (let name in data[0])
 			html.push('<th>' + name + '</th>');
 		html.push('</tr></thead><tbody>');
-		for (var i = 0; i < data.length; i++) {
+		for (let i = 0; i < data.length; i++) {
 			if(!drawData) break;
 			html.push('<tr>');
-			for (var name in data[i]) {
-				var type = typeof data[i][name];
-				var val = type !== "string" && type !== "number" ? "[" + type + "]" : data[i][name];
+			for (let name in data[i]) {
+				let type = typeof data[i][name];
+				let val = type !== "string" && type !== "number" ? "[" + type + "]" : data[i][name];
 				if (val.length > 50)
 					val = val.substring(0, 47) + "...";
 				html.push('<td>' + val + '</td>');
@@ -248,7 +301,7 @@ void(function () {
 		return $("#jSQLMyAdminOverlay").length>0 ? closejSQL() : openjSQL();
 	}
 	function openjSQL() {
-		$('body').append('<div id=\'jSQLMyAdminOverlay\' style=\'position:fixed; top:0; left:0; height:100%; width:100%; overflow-y:auto; z-index: 2147483646; background: rgba(255,255,255,0.99)\'><div id="jsOverlayContainer" style="padding:5px; margin:0;"><br><br><center><h3>Loading jSQLMyAdmin...</h3></center></div></div>');
+		$('body').append('<div id=\'jSQLMyAdminOverlay\' style=\'text-align: left; position:fixed; top:0; left:0; height:100%; width:100%; overflow-y:auto; z-index: 2147483646; background: rgba(255,255,255,0.99)\'><div id="jsOverlayContainer" style="padding:5px; margin:0;"><br><br><center><h3>Loading jSQLMyAdmin...</h3></center></div></div>');
 		$('#openjSQLMyAdmin').remove();
 		$('body').append('<button id=\'closejSQLMyAdmin\' style=\'position:fixed; top:5px; right:5px; z-index:2147483647; opacity:0.7;\'>Close jSQLMyAdmin</button>');
 		$('#closejSQLMyAdmin').button({icons: { primary: "ui-icon-closethick"}});
@@ -268,32 +321,32 @@ void(function () {
 		$('#openjSQLMyAdmin').click(togglejSQLMyAdmin);
 	}
 	function loadresources(resources) {
-		return new Promise(function (callback) {
+		return new Promise((callback)=>{
 			(function recurse() {
 				if (!resources.length) {
 					callback();
 					return;
 				}
-				var resource = resources.shift();
-				for(var i=resource.css.length; i--;)
+				let resource = resources.shift();
+				for(let i=resource.css.length; i--;)
 					document.getElementsByTagName('head')[0].insertAdjacentHTML('beforeend', '<link rel=\'stylesheet\' href=\''+resource.css[i]+'\'>');
 				(function innerRecurse() {
 					if (!resource.js.length) {
 						recurse();
 						return;
 					}
-					var src = resource.js.shift();
-					var script = document.createElement('script');
+					let src = resource.js.shift();
+					let script = document.createElement('script');
 					script.type = 'text/javascript';
 					if (script.readyState) { //IE
-						script.onreadystatechange = function () {
+						script.onreadystatechange = ()=>{
 							if (script.readyState == 'loaded' || script.readyState == 'complete') {
 								script.onreadystatechange = null;
 								setTimeout(innerRecurse, 1000);
 							}
 						};
 					} else { //Others
-						script.onload = function () {
+						script.onload = ()=>{
 							setTimeout(innerRecurse, 1000);
 						};
 					}
