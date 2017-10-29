12bcec33421d8f97959ffffe9f7229d47ebf1c5d
Robert Parham <adelphia@protonmail.ch>
2017-10-29 01:06:47 -0400

    trigger build - improved code climate

--- src/table/jSQLTable.js
+++ src/table/jSQLTable.js
@@ -77,7 +77,8 @@ jSQLTable.prototype.initKeys = function(keys, auto_increment){
 	// Set up keys if key data is provided
 	var key;
 	var keyTypes = ["primary", "unique"];
-	for(var k=0; key=keys[k]; k++){
+	for(var k=0; k<keys.length; k++){
+		key=keys[k];
 		if(!key.hasOwnProperty("column") || (!Array.isArray(key.column) && this.columns.indexOf(key.column) === -1)) return _throw(new jSQL_Error("0010"));
 		if(Array.isArray(key.column)){
 			for(var kk=0; kk<key.column.length; kk++){
@@ -122,7 +123,7 @@ jSQLTable.prototype.renameColumn = function(oldname, newname){
 	for(var n=this.keys.unique.length; n--;){
 		if(this.keys.unique[n].column === oldname) this.keys.unique[n].column = newname;
 		if(Array.isArray(this.keys.unique[n].column))
-			for(var i=this.keys.unique[n].column.length; i--;)
+			for(i=this.keys.unique[n].column.length; i--;)
 				if(this.keys.unique[n].column[i] === oldname) this.keys.unique[n].column[i] = newname;
 	}
 	// Update colmap
@@ -199,7 +200,7 @@ jSQLTable.prototype.normalizeInsertRowData = function(data){
 			if(!data.hasOwnProperty(colname)) continue;
 
 			// If the data row column doesn't exist in the table..
-			if(function(l){ while(l--) if(self.columns[l]==colname) return 0; return 1; }(this.columns.length)){
+			if(function(l){ while(l--) if(self.columns[l]===colname) return 0; return 1; }(this.columns.length)){
 				// ...and there is already an undefined row title...
 				if(this.columns.indexOf("u0") > -1){
 					// ...let the undefined row inherit this title,
@@ -210,7 +211,7 @@ jSQLTable.prototype.normalizeInsertRowData = function(data){
 				}else this.addColumn(colname);
 			}
 
-		for(var n=0; n<this.columns.length; n++)
+		for(n=0; n<this.columns.length; n++)
 			row.push(data[this.columns[n]]);
 	}else return _throw(new jSQL_Error("0015"));
 	return row;
@@ -228,7 +229,7 @@ jSQLTable.prototype.insertRow = function(data, ignore){
 				row[i] = this.auto_inc_seq;
 				this.auto_inc_seq++;
 			} 
-			if(row[i] >= this.auto_inc_seq) this.auto_inc_seq = parseInt(row[i])+1;
+			if(row[i] >= this.auto_inc_seq) this.auto_inc_seq = parseInt(row[i], 10)+1;
 		}
 		row[i] = this.normalizeColumnStoreValue(this.columns[i], row[i]);
 	}
@@ -243,7 +244,8 @@ jSQLTable.prototype.updateKeysOnInsert = function(row, ignore){
 	if(this.keys.primary.column){ 
 		var primary_key_columns = Array.isArray(this.keys.primary.column) ? this.keys.primary.column : [this.keys.primary.column];
 		var pk_col, pk_vals = [];
-		for(var pk=0; pk_col=primary_key_columns[pk]; pk++){
+		for(var pk=0; pk<primary_key_columns.length; pk++){
+			pk_col=primary_key_columns[pk];
 			var primary_index = this.colmap[pk_col];
 			if(null === row[primary_index]){
 				if(ignore === true) return false;
@@ -260,10 +262,12 @@ jSQLTable.prototype.updateKeysOnInsert = function(row, ignore){
 	}
 
 	// Check the unique keys, There may be multiple and they may be compound
-	for(var k=0, ukey; ukey=this.keys.unique[k]; k++){
+	for(var k=0, ukey; k<this.keys.unique.length; k++){
+		ukey=this.keys.unique[k];
 		var key_columns = Array.isArray(ukey.column) ? ukey.column : [ukey.column];
 		var col, vals = [];
-		for(var uk=0; col=key_columns[uk]; uk++){
+		for(var uk=0; uk<key_columns.length; uk++){
+			col=key_columns[uk]
 			var index = this.colmap[col];
 			if(null === row[index]){
 				if(ignore === true) return false;
